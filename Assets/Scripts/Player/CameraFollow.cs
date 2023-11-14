94a5f1920c7ed0ae96c391500b8801508785e48c
Musaab Shalaldeh <musaabsh65@yahoo.com>
2023-11-14 18:06:47 +0300

    Camera Follow
    Added the ability for camera to follow the player

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("References")]
+    public Transform CameraTransform;
+
+    [Header("Settings")]
+    public float followSpeed = 1.0f;
+
+    // Private Variables
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = CalculateOffset(CameraTransform);
+    }
+
+    void Update()
+    {
+        CameraTransform.DOMove(transform.position + offset, followSpeed);
+    }
+
+    Vector3 CalculateOffset(Transform obj)
+    {
+        return obj.position - transform.position;
+    }
+}
