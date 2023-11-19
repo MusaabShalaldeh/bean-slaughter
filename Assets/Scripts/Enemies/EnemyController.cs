746520bfbe9a2a9bf4f06fb1b19f1fc8bd4ad0aa
Musaab Shalaldeh <musaabsh65@yahoo.com>
2023-11-19 22:13:25 +0300

    Enemy Patrol
    added the ability for enemies to patrol around the map

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("References")]
+    public Entity EnemyEntity;
+    public NavMeshAgent agent;
+    public LayerMask groundLayer;
+
+    [Header("Settings")]
+    public float roamRange = 6.0f;
+    public float stoppingDistance = 0.5f;
+
+    // Private Variables
+    Vector3 destination;
+    bool isWalkPointsSet;
+
+    void Start()
+    {
+        agent.speed = EnemyEntity.speed;
+    }
+
+    void Update()
+    {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        if (!isWalkPointsSet) SearchForDestination();
+        if(isWalkPointsSet) agent.SetDestination(destination);
+        if (Vector3.Distance(transform.position, destination) < stoppingDistance) isWalkPointsSet = false;
+    }
+
+    void SearchForDestination()
+    {
+        float x = Random.Range(-roamRange, roamRange);
+        float z = Random.Range(-roamRange, roamRange);
+
+        destination = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if (Physics.Raycast(destination, Vector3.down, groundLayer))
+            isWalkPointsSet = true;
+    }
+}
